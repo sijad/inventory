f0b29ed6c2a720e5967dc787642843b32c71337b
Sajjad Hashemian <hi@hashemian.me>
2021-02-20 22:36:22 +0330

    add coingecko api

---
+++ src/coingecko/hooks.ts
@@ -0,0 +1,34 @@
+import ky from "ky";
+import { useQuery, QueryObserverResult } from "react-query";
+
+type CoinKey = string;
+
+const client = ky.create({
+  prefixUrl: "https://api.coingecko.com/api/v3",
+});
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  market_cap_rank: number;
+  thumb: string;
+  large: string;
+}
+
+export function useCoins(): QueryObserverResult<Record<CoinKey, Coin>> {
+  return useQuery(
+    "search-coins",
+    async () => {
+      const res = await client.get("search").json<{ coins: Coin[] }>();
+      return res.coins.reduce((o, c) => {
+        o[c.symbol] = c;
+        return o;
+      }, {} as Record<CoinKey, Coin>);
+    },
+    {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  );
+}
